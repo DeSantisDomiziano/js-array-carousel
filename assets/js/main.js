7da0ba3bd2a61cafa6bfb3147c0f6fd5e6233121
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-14 18:32:19 +0100

    add work button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,34 @@ const upEl = document.getElementById('up')
 const downEl = document.getElementById('down')
 let mainImg = document.querySelector('.container_main_img')
 let imgEL = [
-    'url(../img/01.webp)',
-    'url(../img/02.webp)',
-    'url(../img/03.webp)',
-    'url(../img/04.webp)',
-    'url(../img/05.webp)',
+    'url(./assets/img/01.webp)',
+    'url(./assets/img/02.webp)',
+    'url(./assets/img/03.webp)',
+    'url(./assets/img/04.webp)',
+    'url(./assets/img/05.webp)',
 ]
 
-
+let i = 0;
 upEl.addEventListener('click', function() {
-
-    for(let i = 0; i < imgEL.length; i++) {
-        
+    
+    i = i - 1   
+    if (i < 0) {
+        i = 4
     }
+    mainImg.style.backgroundImage = imgEL[i]
+    console.log(imgEL[i])
 })
 
-mainImg.style.backgroundImage = imgEL[0]
-console.log(imgEL[0])
-console.log(mainImg.style.backgroundImage)
+downEl.addEventListener('click', function() {
+    
+    i = i + 1   
+    if (i > 4) {
+        i = 0
+    }
+    mainImg.style.backgroundImage = imgEL[i]
+    console.log(imgEL[i])
+})
+    
+
+
+
